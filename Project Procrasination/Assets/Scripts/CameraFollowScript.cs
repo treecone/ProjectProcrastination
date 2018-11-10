43ea90826abebdc67e3b66a9407c183a994af032
Skyler Zartman (RIT Student) <sz5100@ad.rit.edu>
2018-11-10 16:41:26 -0500

    Added camera movement

---
+++ Project Procrasination/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    public GameObject target;
+    public float speed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        gameObject.transform.position = Vector3.Slerp(gameObject.transform.position, new Vector3 (target.transform.position.x, target.transform.position.y, -10), speed * Time.deltaTime);
+	}
+}
