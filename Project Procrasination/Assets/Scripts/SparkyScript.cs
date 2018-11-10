ff38d3e3e47ea06d8dbe9383d096cf65b9de132d
Skyler Zartman (RIT Student) <sz5100@ad.rit.edu>
2018-11-10 18:53:07 -0500

    Revert "Merge branch 'master' of https://github.com/treecone/ProjectProcrastination"
    This reverts commit 8beefa9e799e650d285077fd4f42334266a301f6, reversing
    changes made to 8bc2ecf7a9ab6c6671585383b1fa08d6ad42a4ee.

--- Project Procrasination/Assets/Scripts/SparkyScript.cs
+++ Project Procrasination/Assets/Scripts/SparkyScript.cs
@@ -4,41 +4,13 @@ using UnityEngine;
 
 public class SparkyScript : MonoBehaviour {
 
-    public int movement;
-    public float timeElapsed;
-    public float totalTimeElapsed;
-
-
 	// Use this for initialization
 	void Start () {
-        movement = 1;
-        totalTimeElapsed = 0;
+		
 	}
-
-    // Update is called once per frame
-    void Update() {
-        switch (movement)
-        {
-            case 1:
-                this.transform.Translate(Vector3.right * Time.deltaTime * 3);
-                this.transform.Translate(Vector3.up * Time.deltaTime * Mathf.Sin(totalTimeElapsed * 10));
-                break;
-            case 2:
-                this.transform.Translate(Vector3.left * Time.deltaTime * 3);
-                this.transform.Translate(Vector3.up * Time.deltaTime * Mathf.Sin(totalTimeElapsed * 10));
-                break;
-
-        }
-        timeElapsed += Time.deltaTime;
-        totalTimeElapsed += Time.deltaTime;
-        if(timeElapsed >= 1)
-        {
-            if (movement == 1)
-                movement++;
-            else
-                movement = 1;
-            timeElapsed = 0;
-        }
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
-
 }
