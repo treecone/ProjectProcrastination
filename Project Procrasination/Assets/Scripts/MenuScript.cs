55d46e1acf4808a5da1b3070a5171f781cd276e3
Skyler Zartman (RIT Student) <sz5100@ad.rit.edu>
2018-11-11 12:18:13 -0500

    added menu

---
+++ Project Procrasination/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    //Simple method to start the game!
+    public void StartGame ()
+    {
+        Debug.Log("Starting Game");
+        SceneManager.LoadScene(0);
+    }
+}
