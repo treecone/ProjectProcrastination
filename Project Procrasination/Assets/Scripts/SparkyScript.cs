670a5b28d1e11c42c130e784b58226848247e8fd
Skyler Zartman (RIT Student) <sz5100@ad.rit.edu>
2018-11-11 10:46:52 -0500

    added thing

--- Project Procrasination/Assets/Scripts/SparkyScript.cs
+++ Project Procrasination/Assets/Scripts/SparkyScript.cs
@@ -16,5 +16,12 @@ public class SparkyScript : MonoBehaviour {
     {
         time += 0.1f;
         gameObject.transform.GetChild(0).position = new Vector2(gameObject.transform.position.x + Mathf.Sin(time * -0.33f + 152), gameObject.transform.position.y + Mathf.Sin(time * 0.2f));
+
+        if(Input.GetKey (KeyCode.W) && Input.GetKey(KeyCode.B))
+        {
+            GameObject a = Instantiate(Resources.Load("Whalebe"), GameObject.FindWithTag("Player").transform.position, Quaternion.identity) as GameObject;
+            a.transform.localScale = new Vector3(3, 3, 1);
+
+        }
 	}
 }
