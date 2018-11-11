6ae883c5656d028c4d0cbbb6904611c0d79377cf
Skyler Zartman (RIT Student) <sz5100@ad.rit.edu>
2018-11-10 22:13:32 -0500

    Added time and win event

---
+++ Project Procrasination/Assets/WinScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour {
+
+    public float time;
+    public float percentageOfEssay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += 0.01f;
+        GameObject.Find("Canvas").transform.GetChild(0).transform.rotation = Quaternion.Euler (0,0,90-time);
+        if(time >= 270)
+        {
+            //Game over
+            Debug.Log(percentageOfEssay);
+            SceneManager.LoadScene(1);
+        }
+	}
+}
